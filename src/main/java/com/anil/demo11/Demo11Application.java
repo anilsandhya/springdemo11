6433bb0b33449ea7ce30755ade15f07f53bda8a2
asharma <anilsandhya@gmail.com>
2021-07-11 10:47:21 -0400

    file upload changes

--- src/main/java/com/anil/demo11/Demo11Application.java
+++ src/main/java/com/anil/demo11/Demo11Application.java
@@ -2,10 +2,17 @@ package com.anil.demo11;
 
 import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.anil.demo11.domainobject.fileupload.IStorage;
+import com.anil.demo11.domainobject.fileupload.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Demo11Application {
 
 	public static void main(String[] args) {
@@ -19,4 +26,11 @@ public class Demo11Application {
 		Arrays.asList(names).forEach(System.out::println);
 	}
 
+	@Bean
+	CommandLineRunner init(IStorage storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
